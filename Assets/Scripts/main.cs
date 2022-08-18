466657d1c384ad9227885a00cef72db8b50d2d07
Cppshizoid <alex.krivoschecov@gmail.com>
2022-08-18 23:02:29 +0700

    Update main.cs

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class main : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
